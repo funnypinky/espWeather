ab5f2f945a2958e4ad66b918a1e4b07332f3016f
funnypinky <steffenhasler@googlemail.com>
2021-09-17 10:37:44 +0200

    Update app.ino

--- app.ino
+++ app.ino
@@ -9,3 +9,38 @@ static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
 
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = ( area->x2 - area->x1 + 1 );
+    uint32_t h = ( area->y2 - area->y1 + 1 );
+
+    tft.startWrite();
+    tft.setAddrWindow( area->x1, area->y1, w, h );
+    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    tft.endWrite();
+
+    lv_disp_flush_ready( disp );
+}
+
+void setup()
+{
+    Serial.begin( 115200 ); /* prepare for possible serial debug */
+
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.println( LVGL_Arduino );
+    Serial.println( "I am LVGL_Arduino" );
+
+    lv_init();
+    tft.begin();          /* TFT init */
+    tft.setRotation( 3 ); /* Landscape orientation, flipped */
+
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+}
