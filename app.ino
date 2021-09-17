0b7b3db14d781d4e377442f651257ea22b04f5b4
funnypinky <steffenhasler@googlemail.com>
2021-09-17 10:34:59 +0200

    Create app.ino

---
+++ app.ino
@@ -0,0 +1,11 @@
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+
+/*Change to your screen resolution*/
+static const uint16_t screenWidth  = 480;
+static const uint16_t screenHeight = 320;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenWidth * 10 ];
+
+TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
